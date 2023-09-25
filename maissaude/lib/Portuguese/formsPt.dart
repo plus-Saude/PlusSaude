d3b4fee28a7618f627a91e17968bfcd0270e29da
Lucas Araujo <lucasaraujoo44.la@gmail.com>
2023-09-25 16:18:27 -0300

    Feat: Criação do formulário e as respostas em portugues

---
+++ maissaude/lib/Portuguese/formsPt.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:maissaude/Portuguese/resultadoPt.dart';
+
+class MeuForm extends StatefulWidget {
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MeuForm> {
+  bool? receberNotificacoes;
+  bool? receberAtualizacoes;
+  bool? vacinaMeningite;
+
+  void limparCampos() {
+    setState(() {
+      receberNotificacoes = null;
+      receberAtualizacoes = null;
+      vacinaMeningite = null;
+    });
+  }
+
+  void _enviarFormulario() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ResultPage(
+          receberNotificacoes: receberNotificacoes,
+          receberAtualizacoes: receberAtualizacoes,
+          vacinaMeningite: vacinaMeningite,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Formulário de Sim ou Não'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Você possui a vacina da Febre amarela?'),
+            Row(
+              children: [
+                Text('Sim'),
+                Radio<bool>(
+                  value: true,
+                  groupValue: receberNotificacoes,
+                  onChanged: (value) {
+                    setState(() {
+                      receberNotificacoes = value!;
+                    });
+                  },
+                ),
+                Text('Não'),
+                Radio<bool>(
+                  value: false,
+                  groupValue: receberNotificacoes,
+                  onChanged: (value) {
+                    setState(() {
+                      receberNotificacoes = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text('Você possui a vacina do Sarampo?'),
+            Row(
+              children: [
+                Text('Sim'),
+                Radio<bool>(
+                  value: true,
+                  groupValue: receberAtualizacoes,
+                  onChanged: (value) {
+                    setState(() {
+                      receberAtualizacoes = value!;
+                    });
+                  },
+                ),
+                Text('Não'),
+                Radio<bool>(
+                  value: false,
+                  groupValue: receberAtualizacoes,
+                  onChanged: (value) {
+                    setState(() {
+                      receberAtualizacoes = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Text('Você possui a vacina da Meningite?'),
+            Row(
+              children: [
+                Text('Sim'),
+                Radio<bool>(
+                  value: true,
+                  groupValue: vacinaMeningite,
+                  onChanged: (value) {
+                    setState(() {
+                      vacinaMeningite = value!;
+                    });
+                  },
+                ),
+                Text('Não'),
+                Radio<bool>(
+                  value: false,
+                  groupValue: vacinaMeningite,
+                  onChanged: (value) {
+                    setState(() {
+                      vacinaMeningite = value!;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _enviarFormulario,
+              child: Text('Enviar'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Função para limpar os campos
+                limparCampos();
+              },
+              child: Text('Limpar'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/main');
+              },
+              child: Text('Voltar a tela principal'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
